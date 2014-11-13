f34a07a1bf5c55132b60d056bf22e2078ebb6304
Oscar <Oscar@Oscar-PC>
2014-11-13 22:20:02 +0100

    fallo en fibonacci

---
+++ src/ejercicio5/Ejercicio5.java
@@ -0,0 +1,19 @@
+
+package ejercicio5;
+
+/**
+ *
+ * @author Oscar
+ */
+public class Ejercicio5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        SerieNum par=new SerieNum();
+        par.calcularSeries();
+
+    }
+    
+}
---
+++ src/ejercicio5/SerieNum.java
@@ -0,0 +1,67 @@
+package ejercicio5;
+
+import javax.swing.JOptionPane;
+
+/*
+ * @author Oscar
+ */
+public class SerieNum {
+
+    private long num = this.pedirNum();
+
+    public long pedirNum() {
+
+        do {
+            String dato = JOptionPane.showInputDialog("Introducir número de Elementos");
+            num = Long.parseLong(dato);
+        } while (num <= 0);
+        return num;
+    }
+
+    public String seriePar() {
+
+        long par = 0;
+        String seriepar = "";
+
+        for (int i = 0; i < num; i++) {
+            par = par + 2;
+            seriepar += " " + par;
+        }
+        return seriepar;
+    }
+
+    public String serieImparPar() {
+
+        String serie = "";
+
+        for (int i = 1; i < num + 1; i++) {
+            if (i % 2 == 0) {
+                serie = serie + "+" + i;
+            }
+            if (i % 2 == 1) {
+                serie += "-" + i;
+            }
+        }
+        return serie;
+    }
+
+    public String serieFibonacci() {
+
+        long fib1 = 0;
+        long fib2 = 1;
+        String serie = "";
+
+        for (int i = 0; i+2 < num; i++) {
+            fib1 += fib2;
+            fib2 += fib1;
+            serie += " " + fib1 + " " + fib2;
+
+        }
+        return serie;
+    }
+
+    public void calcularSeries() {
+        String serie = ("Serie 1: " + this.seriePar() + "\nSerie 2: " + this.serieImparPar() + "\nSerie 3: " + this.serieFibonacci());
+        JOptionPane.showMessageDialog(null, serie);
+    }
+}
